f128aec366480104c9c4304657a46e13af487c34
Unknown <jjsteegm@student.avans.nl>
2018-03-01 13:35:26 +0100

    changed include file

--- MicroControllersWeek3/LCDmodual/src/LCD.h
+++ MicroControllersWeek3/LCDmodual/src/LCD.h
@@ -6,18 +6,18 @@
  */ 
 
 
-void lcd_strobe_lcd_e(void);
-void init_4bits_mode(void);
-void lcd_write_string(char *str);
-void lcd_write_data(unsigned char byte);
-void lcd_write_cmd(unsigned char byte);
-int lcd_test_main( void );
+
 
 #ifndef FUNCTIONS_H_
 #define FUNCTIONS_H_
 
 
-
+void lcd_strobe_lcd_e(void);
+void init_4bits_mode(void);
+void lcd_write_string(char *str);
+void lcd_write_data(unsigned char byte);
+void lcd_write_command(unsigned char byte);
+int lcd_test_main( void );
 
 
 #endif /* FUNCTIONS_H_ */
