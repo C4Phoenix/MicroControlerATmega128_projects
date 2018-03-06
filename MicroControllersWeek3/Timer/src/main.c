7a20b587abef694914e55924128fe18473865932
Unknown <rm.hobbel@telfort.nl>
2018-03-06 16:56:27 +0100

    Updated Comments, tweaked code

--- MicroControllersWeek3/Timer/src/main.c
+++ MicroControllersWeek3/Timer/src/main.c
@@ -9,19 +9,23 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-int overflowCount = 0;
+int countOverFlow = 0;
+int highMode = 1;
 
 int main(void)
 {
 	DDRD = 0xFF;//Set d to output
 		
-	TCNT2 = 0;//Timer/Counter Register, value of timer is soterd here, Initialize counter
-	OCR2 = 125;
+	TCNT2 = 0;//Timer/Counter Register, value of timer is stored here, Initialize counter
+	OCR2 = 100; //Compare value timer 2, interrupts after compare is true, made value higher so its beter visible on camera
 	TIMSK |= (1 << 7);//Timer/Counter Interrupt Mask, turn the 1 in 0b0100 0000 on to enable overflow interrupts
 	SREG |= (1 << 7); //Set global interrupt bit
 	
-	TCCR2 = 0b00001101; // Counting via D, prescaler:
-	//TCCR2 = 0b;
+	TCCR2 = 0b00001101; // Counting via D, prescaler: 1024
+	//TCCR2 = 0b00001001; // prescaler: none
+	//TCCR2 = 0b00001010; // prescaler: 8
+	//TCCR2 = 0b00001011; // prescaler: 32
+	//TCCR2 = 0b00001100; // prescaler: 256
 	sei();//activate interrupts
 	
 
@@ -32,13 +36,21 @@ int main(void)
 }
 
 
-ISR( TIMER2_COMP_vect ) //Interrupt for timer 2
+ISR( TIMER2_COMP_vect ) //Interrupt for timer 2, after a compare
 {
-	overflowCount++;
-	overflowCount = overflowCount % 40;
-	if(overflowCount == 0) {
-		PORTD = 0b10000000;
-		} else if(overflowCount == 15) {
-		PORTD = 0b00000000;
+	//moet 15ms hoog, 25ms laag
+	countOverFlow++;
+	if(highMode) {
+		if(countOverFlow == 15) {
+			PORTD = 0b01000000;//laag
+			highMode = 0;//turn highmode off
+			countOverFlow =0;
+		}
+	} else {
+		if (countOverFlow == 25) {
+			PORTD = 0b10000000;//hoog
+			highMode = 1; //turn highmode on
+			countOverFlow = 0;
+		}
 	}
 }
