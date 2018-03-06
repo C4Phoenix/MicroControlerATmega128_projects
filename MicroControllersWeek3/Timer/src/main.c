e2e7530c98ffb74b2b520e1b4528f5e8ec833378
Unknown <rm.hobbel@telfort.nl>
2018-03-06 15:52:49 +0100

    Added Some Prescaler Values

--- MicroControllersWeek3/Timer/src/main.c
+++ MicroControllersWeek3/Timer/src/main.c
@@ -8,22 +8,20 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-//#include "LCD.h"
 
 int overflowCount = 0;
 
 int main(void)
 {
-	//DDRB = 0xFF;//Set B, C & D to output
-	//DDRC = 0xFF;
 	DDRD = 0xFF;//Set d to output
 		
 	TCNT2 = 0;//Timer/Counter Register, value of timer is soterd here, Initialize counter
 	OCR2 = 125;
 	TIMSK |= (1 << 7);//Timer/Counter Interrupt Mask, turn the 1 in 0b0100 0000 on to enable overflow interrupts
-	//SREG |= (1 << 7); //Set global interrupt bit
+	SREG |= (1 << 7); //Set global interrupt bit
 	
-	TCCR2 = 0b00001011; // Counting via D
+	TCCR2 = 0b00001101; // Counting via D, prescaler:
+	//TCCR2 = 0b;
 	sei();//activate interrupts
 	
 
