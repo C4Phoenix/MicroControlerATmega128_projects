a6d7e328255c8869464c0d9556778fb641d1567d
Unknown <rm.hobbel@telfort.nl>
2018-03-07 11:03:08 +0100

    Added Extra commentary

--- MicroControllersWeek4/MicroControllersWeek4/src/main.c
+++ MicroControllersWeek4/MicroControllersWeek4/src/main.c
@@ -14,7 +14,9 @@ int main(void) {
 	DDRF = 0x00;				//PORTF for input ADC
 	DDRA = 0xFF;				//PORTA for output
 	DDRB = 0xFF;				//PORTB for output
-	ADMUX = 0b01100000;			//Bit 7 en 6 zorgen voor de ADC output.  Bit 5 is ADLAR die op 1 wordt gezet. Bit 4 tot en met 0 is voor het kiezen van pinF0 tot en met pinF7.
+	ADMUX = 0b01100000;			//Bit 7 en 6 zorgen voor de ADC output.  
+								//Bit 5 is ADLAR die op 1 wordt gezet. 
+								//Bit 4 tot en met 0 is voor het kiezen van pinF0 tot en met pinF7.
 	ADCSRA = 0b11100110;		//Bit 7 enable ADC. Bit 6 ADC start conversie. Bit 5 Free running aan of uit. Bit 2 t/m 0 is voor de prescaler (64).
     while (1) {
 		PORTB = ADCL;			// Show MSB/LSB (bit 10:0) of ADC
