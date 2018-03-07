54a5a799598b9585967700d6581a12518318737e
Unknown <rm.hobbel@telfort.nl>
2018-03-07 11:05:17 +0100

    added more comments

--- MicroControllersWeek4/MicroControllersWeek4/src/main.c
+++ MicroControllersWeek4/MicroControllersWeek4/src/main.c
@@ -20,7 +20,8 @@ int main(void) {
 	ADCSRA = 0b11100110;		//Bit 7 enable ADC. Bit 6 ADC start conversie. Bit 5 Free running aan of uit. Bit 2 t/m 0 is voor de prescaler (64).
     while (1) {
 		PORTB = ADCL;			// Show MSB/LSB (bit 10:0) of ADC
-		PORTA = ADCH;
+		PORTA = ADCH;			// ADCH reads the high byte, ADCL reads the low byte, 
+								//ADCW read the whole "word"->needs checking if true
 		wait(100);
     }
 }
