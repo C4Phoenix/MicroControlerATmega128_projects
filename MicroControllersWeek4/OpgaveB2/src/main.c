475acd09ea3d6ba11ff89c347147a473a735885f
Unknown <rm.hobbel@telfort.nl>
2018-03-07 11:59:03 +0100

    Finished B2, Needs verification

--- MicroControllersWeek4/OpgaveB2/src/main.c
+++ MicroControllersWeek4/OpgaveB2/src/main.c
@@ -13,17 +13,16 @@ void wait(int);
 int main(void) {	
 	DDRF = 0x00;				//PORTF for input ADC
 	DDRA = 0xFF;				//PORTA for output
-	DDRB = 0xFF;				//PORTB for output
+	DDRB = 0xFF;
 
 	ADMUX = 0b11100000;			//Bit 7 en 6 zorgen voor de ADC output.  
 								//Bit 5 is ADLAR die op 1 wordt gezet. 
-								//Bit 4 tot en met 0 is voor het kiezen van pinF0 tot en met pinF7.
-	ADCSRA = 0b11100110;		//Bit 7 enable ADC. Bit 6 ADC start conversie. Bit 5 Free running aan of uit. Bit 2 t/m 0 is voor de prescaler (64).
+	ADCSRA = 0b10000110;		//Bit 7 enable ADC. Bit 6 ADC start conversie. Bit 5 Free running aan of uit. Bit 2 t/m 0 is voor de prescaler (64).
     while (1) {
-		PORTA = ADCL;			// Show MSB/LSB (bit 10:0) of ADC
-		PORTB = ADCH;			// ADCH reads the high byte, ADCL reads the low byte, 
-								//ADCW read the whole "word"->needs checking if true
-		wait(100);
+		ADCSRA |= 0b01000000; 
+		while(ADCSRA & 0b01000000);
+		PORTA = ADCH;
+		wait(500);
     }
 }
 
