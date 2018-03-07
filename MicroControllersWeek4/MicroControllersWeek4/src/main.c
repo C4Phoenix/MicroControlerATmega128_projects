298bbda6fe1b2821980e9aba8cf0944ce5064e11
Unknown <rm.hobbel@telfort.nl>
2018-03-07 11:50:36 +0100

    Updated B1, mistake in init for ADC

--- MicroControllersWeek4/MicroControllersWeek4/src/main.c
+++ MicroControllersWeek4/MicroControllersWeek4/src/main.c
@@ -14,7 +14,7 @@ int main(void) {
 	DDRF = 0x00;				//PORTF for input ADC
 	DDRA = 0xFF;				//PORTA for output
 	DDRB = 0xFF;				//PORTB for output
-	ADMUX = 0b01100000;			//Bit 7 en 6 zorgen voor de ADC output.  
+	ADMUX = 0b11100000;			//Bit 7 en 6 zorgen voor de ADC output.  
 								//Bit 5 is ADLAR die op 1 wordt gezet. 
 								//Bit 4 tot en met 0 is voor het kiezen van pinF0 tot en met pinF7.
 	ADCSRA = 0b11100110;		//Bit 7 enable ADC. Bit 6 ADC start conversie. Bit 5 Free running aan of uit. Bit 2 t/m 0 is voor de prescaler (64).
--- MicroControllersWeek4/OpgaveB2/src/main.c
+++ MicroControllersWeek4/OpgaveB2/src/main.c
@@ -1,40 +1,34 @@
-/**
- * \file
- *
- * \brief Empty user application template
- *
- */
-
-/**
- * \mainpage User Application template doxygen documentation
- *
- * \par Empty user application template
- *
- * Bare minimum empty user application template
- *
- * \par Content
- *
- * -# Include the ASF header files (through asf.h)
- * -# "Insert system clock initialization code here" comment
- * -# Minimal main function that starts with a call to board_init()
- * -# "Insert application code here" comment
- *
- */
-
 /*
- * Include header files for all drivers that have been imported from
- * Atmel Software Framework (ASF).
- */
-/*
- * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
- */
-#include <asf.h>
+ * Opdrachtb2.c
+ *
+ * Created: 7-3-2018 11:48:27
+ * Author : Robin Hobbel
+ */ 
+#define F_CPU 8000000
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+void wait(int);
 
-int main (void)
-{
-	/* Insert system clock initialization code here (sysclk_init()). */
+int main(void) {	
+	DDRF = 0x00;				//PORTF for input ADC
+	DDRA = 0xFF;				//PORTA for output
+	DDRB = 0xFF;				//PORTB for output
 
-	board_init();
+	ADMUX = 0b11100000;			//Bit 7 en 6 zorgen voor de ADC output.  
+								//Bit 5 is ADLAR die op 1 wordt gezet. 
+								//Bit 4 tot en met 0 is voor het kiezen van pinF0 tot en met pinF7.
+	ADCSRA = 0b11100110;		//Bit 7 enable ADC. Bit 6 ADC start conversie. Bit 5 Free running aan of uit. Bit 2 t/m 0 is voor de prescaler (64).
+    while (1) {
+		PORTA = ADCL;			// Show MSB/LSB (bit 10:0) of ADC
+		PORTB = ADCH;			// ADCH reads the high byte, ADCL reads the low byte, 
+								//ADCW read the whole "word"->needs checking if true
+		wait(100);
+    }
+}
 
-	/* Insert application code here, after the board has been initialized. */
+void wait( int ms ) {
+	for (int tms=0; tms<ms; tms++) {
+		_delay_ms( 1 );			// library function (max 30 ms at 8MHz)
+	}
 }
