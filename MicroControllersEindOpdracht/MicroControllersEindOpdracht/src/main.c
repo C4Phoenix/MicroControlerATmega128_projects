338c5c2aff7f498aa148d50be57064d05b50b994
Unknown <rm.hobbel@telfort.nl>
2018-03-22 15:56:06 +0100

    Added code, can now write images

--- MicroControllersEindOpdracht/MicroControllersEindOpdracht/src/main.c
+++ MicroControllersEindOpdracht/MicroControllersEindOpdracht/src/main.c
@@ -1,95 +1,46 @@
-/* ---------------------------------------------------------------------------
-** This software is in the public domain, furnished "as is", without technical
-** support, and with no warranty, express or implied, as to its usefulness for
-** any purpose.
-**
-** ledmatrix.c
-**
-** Beschrijving:	Simple HT16K33 Ledmatix demo.
-** Target:			AVR mcu
-** Build:			avr-gcc -std=c99 -Wall -O3 -mmcu=atmega128 -D F_CPU=8000000UL -c ledmatrix.c
-**					avr-gcc -g -mmcu=atmega128 -o ledmatrix.elf ledmatrix.o
-**					avr-objcopy -O ihex ledmatrix.elf ledmatrix.hex 
-**					or type 'make'
-** Author: 			dkroeske@gmail.com
-** -------------------------------------------------------------------------*/
-
 #define F_CPU 8000000
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdio.h>
 
 void setBrightness(int);
-void sendData(int);
+void sendDataD1(int);
+void sendDataD2(int);
+void setupRegister(void);
+void setChipPins(void);
+void setupDisplay(void);
+void setLedsInRowD1(int, int);
+void clearDisplay1(void);
+void clearDisplay2(void);
+void setLedsInRowD2(int, int);
+void printImageD1(unsigned char*);
+//unsigned char* mirror_img(unsigned char*);
 
-
-
-/******************************************************************/
 void twi_init(void)
-/* 
-short:			Init AVR TWI interface and set bitrate
-inputs:			
-outputs:	
-notes:			TWI clock is set to 100 kHz
-Version :    	DMK, Initial code
-*******************************************************************/
 {
-	TWSR = 0;
+	TWSR = 0;	//Set Status to 0
 	TWBR = 32;	 // TWI clock set to 100kHz, prescaler = 0
 }
 
-/******************************************************************/
 void twi_start(void)
-/* 
-short:			Generate TWI start condition
-inputs:		
-outputs:	
-notes:			
-Version :    	DMK, Initial code
-*******************************************************************/
 {
-	TWCR = (0x80 | 0x20 | 0x04);
+	TWCR = (0x80 | 0x20 | 0x04); //1010 0100 //bit 5 -> startcondition for TWI, bit 7 -> Interrupt flag, bit 3 -> Collision flag
 	while( 0x00 == (TWCR & 0x80) );
 }
 
-/******************************************************************/
 void twi_stop(void)
-/* 
-short:			Generate TWI stop condition
-inputs:		
-outputs:	
-notes:			
-Version :    	DMK, Initial code
-*******************************************************************/
 {
-	TWCR = (0x80 | 0x10 | 0x04);
+	TWCR = (0x80 | 0x10 | 0x04); //1001 0100 //bit 4 -> Stop condition for TWI, 
 }
 
-/******************************************************************/
 void twi_tx(unsigned char data)
-/* 
-short:			transmit 8 bits data
-inputs:		
-outputs:	
-notes:			
-Version :    	DMK, Initial code
-*******************************************************************/
 {
 	TWDR = data;
 	TWCR = (0x80 | 0x04);
 	while( 0 == (TWCR & 0x80) );
 }
 
-/******************************************************************/
 void wait( int ms )
-/* 
-short:			Busy wait number of millisecs
-inputs:			int ms (Number of millisecs to busy wait)
-outputs:	
-notes:			Busy wait, not very accurate. Make sure (external)
-				clock value is set. This is used by _delay_ms inside
-				util/delay.h
-Version :    	DMK, Initial code
-*******************************************************************/
 {
 	for (int i=0; i<ms; i++)
 	{
@@ -97,74 +48,152 @@ Version :    	DMK, Initial code
 	}
 }
 
-/******************************************************************/
+
 int main( void )
-/* 
-short:			main() loop, entry point of executable
-inputs:			
-outputs:	
-notes:			Looping forever, trashing the HT16K33
-Version :    	DMK, Initial code
-*******************************************************************/
 {
 	int brightness = 0;
-	
-	twi_init();		// Init TWI interface
-
-	// Init HT16K22. Page 32 datasheet
-	twi_start();
-	twi_tx(0xE0);	// Display I2C addres + R/W bit //1110 0000
-	twi_tx(0x21);	// Internal osc on (page 10 HT16K33) // 0010 0001 -> System Setup Register (page 10)
-	twi_stop();
-
-	twi_start();
-	twi_tx(0xE0);	// Display I2C address + R/W bit //1110 0000
-	twi_tx(0xA0);	// HT16K33 pins all output //1010 0000 ->ROW/INT set (page 10)
-	twi_stop();
-
-	twi_start();
-	twi_tx(0xE0);	// Display I2C address + R/W bit //1110 0000
-	twi_tx(0b11101111);	// Display Dimming 4/16 duty cycle //1110 0011 -> Digital Dimming Data Input (page 15)
-	twi_stop();
-
-	twi_start();
-	twi_tx(0xE0);	// Display I2C address + R/W bit //1110 0000
-	twi_tx(0x81);	// Display OFF - Blink On //1000 0001 ->Display Setup Register (page 11)
-	twi_stop();
-
+	unsigned char image[8]= {
+		0b00110011,
+		0b00110011,
+		0b00110011,
+		0b00110011,
+		0b00110011,
+		0b00110011,
+		0b00110011,
+		0b00110011,
+	};
+	twi_init();
+	setupRegister();
+	setChipPins();
+	setupDisplay();
+	clearDisplay1();
+	clearDisplay2();
+	printImageD1(image);
+	wait(1000);
+	for (int i = 0; i<=16;i +=2){
+		for (int j = 0 ; j <=255;j++)
+		{
+			wait(5);
+			setLedsInRowD1(i,j);
+			wait(5);
+			setLedsInRowD2(16-i,254-j);
+		}
+	}
 	while (1)
 	{
 		brightness++;
 		setBrightness(brightness%16);
-		twi_start();
-		twi_tx(0xE0);	// Display I2C addres + R/W bit //1110 0000
-		twi_tx(0x00);	// Address
-		twi_tx(0x00);	// data
-		twi_stop();
-
-		wait(500);	
-
-		twi_start();
-		twi_tx(0xE0);	// Display I2C addres + R/W bit //1110 0000
-		twi_tx(0x00);	// Address
-		twi_tx(0x01);	// data
-		twi_stop();	
-
-		wait(500);
+		wait(100);
 	}
 
 	return 1;
 }
 
-void sendData(int data){
+void sendDataD1(int data){
 	twi_start();
 	twi_tx(0xE0);
 	twi_tx(data);
 	twi_stop();
 }
 
-//give a value from 
-void setBrightness(int brightness){
-	sendData((0b11100000+(brightness)));
+void sendDataD2(int data){
+	twi_start();
+	twi_tx(0xE4);
+	twi_tx(data);
+	twi_stop();
 }
 
+
+void setLedsInRowD1(int row, int data){
+	twi_start();
+	twi_tx(0xE0);	// Display I2C addres + R/W bit //1110 0000	
+	twi_tx(row);	// Address
+	twi_tx(data);	// data
+	twi_stop();
+}
+
+void setLedsInRowD2(int row, int data){
+		twi_start();
+		twi_tx(0xE4);	// Display I2C addres + R/W bit //1110 0000
+		twi_tx(row);	// Address
+		twi_tx(data);	// data
+		twi_stop();
+}
+
+void clearDisplay1(){
+	setLedsInRowD1(0x00,0);
+	setLedsInRowD1(0x02,0);
+	setLedsInRowD1(0x04,0);
+	setLedsInRowD1(0x06,0);
+	setLedsInRowD1(0x08,0);
+	setLedsInRowD1(0x0A,0);
+	setLedsInRowD1(0x0C,0);
+	setLedsInRowD1(0x0E,0);
+}
+
+void clearDisplay2(){
+	setLedsInRowD2(0x00,0);
+	setLedsInRowD2(0x02,0);
+	setLedsInRowD2(0x04,0);
+	setLedsInRowD2(0x06,0);
+	setLedsInRowD2(0x08,0);
+	setLedsInRowD2(0x0A,0);
+	setLedsInRowD2(0x0C,0);
+	setLedsInRowD2(0x0E,0);
+}
+
+
+//give a value from 
+void setBrightness(int brightness){
+	sendDataD1((0b11100000+(brightness)));
+	sendDataD2((0b11100000+(brightness)));
+}
+
+void setupRegister(){
+	sendDataD1(0b00100001);
+	sendDataD2(0b00100001);
+}
+
+void setChipPins(){
+	sendDataD1(0b10100000);
+	sendDataD2(0b10100000);
+}
+
+void setupDisplay(){
+	sendDataD1(0b10000001);
+	sendDataD2(0b10000001);
+}
+/*
+unsigned char* mirror_img(unsigned char* imge){
+	unsigned char* toReturn = malloc(sizeof(unsigned char)*sizeof(imge));
+
+	short len = 0;
+	for(len =0; len< sizeof(imge); len++)
+	{
+		unsigned char toMir = imge[len];
+		int i=7;
+		unsigned char j=0;
+		unsigned char temp=0;
+
+		while(i>=0){
+			temp |= (( toMir >> j ) &1 ) << i;
+			i--;
+			j++;
+		}
+		toReturn[len] = temp;
+	}
+	return toReturn;
+}
+*/
+
+void printImageD1(unsigned char* img){
+	for(int row = 0; row <= 7; row++) {
+		setLedsInRowD1((row*2),img[row]);
+	}
+}
+
+void printImageD2(unsigned char* img){
+	for(int row = 0; row <= 7; row++) {
+		setLedsInRowD2((row*2),img[row]);
+	}
+}
